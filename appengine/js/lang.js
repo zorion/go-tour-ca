2d1af23fe6f1b6b141299c9dbd10319a6cfee669
zorion <zorionk@gmail.com>
2013-03-10 21:37:40 +0100

    Translate the title for Compile and Options

--- appengine/js/lang.js
+++ appengine/js/lang.js
@@ -8,6 +8,8 @@ var tr = {
 	"format": "Donar format al codi",
 	"kill": "Acabar execució",
 	"run": "Executar",
+	"compile": "Compilar i executar",
+	"more": "Opcions",
 	"toc": "Taula de continguts",
 	"prev": "Anterior",
 	"next": "Següent",
